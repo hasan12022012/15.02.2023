a3d0cc5d4062f085dbcbe30a85a79a1320337eb2
Hasan Huseynli <hasansh@code.edu.az>
2023-02-15 07:22:06 +0400

    task2Added

---
+++ task2/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            while(true)
+            {
+                Console.WriteLine("Fullname:");
+                string fullname=Console.ReadLine();
+                if (Student.CheckFullName(fullname))
+                {
+                    std.Fullname = fullname;
+                    break;
+                }
+                Console.WriteLine("Yeniden daxil edin");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("GroupNo;");
+                string groupNo=Console.ReadLine();
+                if (Student.CheckGroupNo(groupNo))
+                {
+                    std.GroupNo=groupNo;
+                    break;
+                }
+                Console.WriteLine("Yeniden daxil edin")
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Age:");
+                int age;
+                if(int.TryParse(Console.ReadLine(), out age))
+                {
+                    std.Age = age;
+                    break;
+                }
+                Console.WriteLine("Yeniden daxil edin");
+            }
+            Console.WriteLine($"Student: {std.Fullname}, GroupNo:{std.GroupNo}, Age:{std.Age}");
+        }
+    }
+}
---
+++ task2/task2/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task2
+{
+    internal class Student
+    {
+        public string Fullname { get; set; }
+        public string GroupNo { get; set; }
+        public byte Age { get; set; }
+
+        public static bool CheckFullName(string fullName)
+        {
+            string[] names= fullName.Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+            if(names.Length > 0 )
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(names[0]) || string.IsNullOrEmpty(names[1]))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckGroupNo(int groupNo)
+        {
+            if (groupNo >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
